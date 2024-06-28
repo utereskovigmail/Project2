38ef3842317c3555fcf58db8bf9632e114680db5
utereskovigmail <93907050+utereskovigmail@users.noreply.github.com>
2024-06-28 15:27:06 +0200

    Update project2.cpp

--- project2.cpp
+++ project2.cpp
@@ -3,35 +3,44 @@
 
 using namespace std;
 
+
 int Count(char str1[], char str2[]){
-    int len1 = strlen(str1);
-    int len2 = strlen(str2);
-    int count = 0;
+    int len1=strlen(str1);
+    int len2=strlen(str2);
+    bool b = true; 
+    int count=0;
 
-    for(int i = 0; i <= len1 - len2; i++){
-        bool found = true;
-        for(int j = 0; j < len2; j++){
-            if(str1[i + j] != str2[j]){
-                found = false;
-                break;
-            }
+    for(int i=0; i<=len1-len2; i++){
+       
+        if(i == 0 || str1[i - 1] == ' '){
+             b=true;
+            
+            for(int j=0; j<len2; j++){
+                if(str1[i + j] != str2[j]){
+                    b=false;
+                    break;
+                }
         }
-        if(found){
+       
+    if(b){
             count++;
-            i += len2 - 1; 
         }
+        }
+     
+        
     }
-
     return count;
 }
 
 int main(){
-    char str1[30];
-    cin.getline(str1, 30);
-    char str2[10];
-    cin.getline(str2, 10);
+    char str1[50];
+    cin.getline(str1,50);
 
-    cout << Count(str1, str2);
+
+    char str2[10];
+    cin.getline(str2,10);
+
+    cout<<Count(str1,str2);
 
     return 0;
 }
